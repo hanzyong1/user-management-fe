29ff3c8511dc3711b471269ececc242830c6e15c
hanzhiyong <hanzyong1@gmail.com>
2026-03-18 20:25:42 +0800

    add Sidenav component

---
+++ app/components/SideNav.tsx
@@ -0,0 +1,55 @@
+import { Box, NavLink, Image, Space, Text, Button, Stack } from "@mantine/core";
+import Link from "next/link";
+import NextImage from "next/image";
+import TestLogo from "@/public/test.png";
+import { IconLogout } from "@tabler/icons-react";
+import { paths } from "@/app/routes";
+
+export default function SideNav() {
+  // Navigation paths
+  const items = paths.map((path) => {
+    return (
+      <NavLink
+        component={Link}
+        href={path.path}
+        key={path.pathName}
+        label={path.pathName}
+        fw="bold"
+        leftSection={<path.icon size={16} stroke={1.5} />}
+      />
+    );
+  });
+
+  return (
+    <Stack p="lg" h="100vh" justify="space-between">
+      <div>
+        <Box style={{ display: "flex", justifyContent: "center" }}>
+          <Image
+            component={NextImage}
+            src={TestLogo}
+            alt="Test Logo"
+            w="60%"
+            h="auto"
+            fit="contain"
+            priority
+          />
+        </Box>
+
+        <Space h="md" />
+        <Box>{items}</Box>
+      </div>
+      <div>
+        <Button
+          variant="transparent"
+          c="#828282"
+          leftSection={<IconLogout size={14} />}
+          // onClick={handleLogout}
+        >
+          <Text size="xs" fw="bold">
+            Logout
+          </Text>
+        </Button>
+      </div>
+    </Stack>
+  );
+}
