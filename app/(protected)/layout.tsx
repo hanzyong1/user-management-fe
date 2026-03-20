61edd63702948aa210c11070591414337534879a
hanzhiyong <hanzyong1@gmail.com>
2026-03-20 15:58:19 +0800

    refactor protected layout to validate user using useAuth

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,7 +1,41 @@
+"use client";
+
+import { useEffect } from "react";
+import { useAuth } from "@/app/lib/useAuth";
+import { useRouter } from "next/navigation";
+import { Center, Loader } from "@mantine/core";
+
 export default function ProtectedLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  type AuthState = {
+    loading: boolean;
+    authenticated: boolean;
+  };
+
+  const { loading, authenticated } = useAuth() as AuthState;
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!loading && !authenticated) {
+      router.replace("/login");
+    }
+  }, [loading, authenticated, router]);
+
+  if (loading) {
+    return (
+      <Center h="100vh">
+        <Loader />
+      </Center>
+    );
+  }
+
+  if (!authenticated) {
+    return null; // prevent rendering children while redirecting
+  }
+
   return <>{children}</>;
 }
